436c94707bd96c9d67adbb6b0d32e08733a59bc6
kurowskitomek <tomekkurowski2003@gmail.com>
2022-11-15 22:30:40 +0100

    Added basic auth DTOs.

---
+++ DataModel/DTOs/AuthResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OceanBattle.DataModel.DTOs
+{
+    /// <summary>
+    /// Pair of tokens issued after succesfull auth.
+    /// </summary>
+    public class AuthResponse
+    {
+        /// <summary>
+        /// JWT Bearer token issued to user after succesful auth.
+        /// </summary>
+        public string? BearerToken { get; set; }
+
+        /// <summary>
+        /// Refresh token issued to user along JWT after succesful auth.
+        /// </summary>
+        public string? RefreshToken { get; set; }
+    }
+}
---
+++ DataModel/DTOs/LogInRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OceanBattle.DataModel.DTOs
+{
+    public class LogInRequest
+    {
+        /// <summary>
+        /// User's password.
+        /// </summary>
+        public string? Password { get; set; }
+        
+        /// <summary>
+        /// User's email.
+        /// </summary>
+        public string? Email { get; set; }
+    }
+}
