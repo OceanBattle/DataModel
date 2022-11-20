7da85d104b37dd3044932df28fdbd201856d7593
kurowskitomek <tomekkurowski2003@gmail.com>
2022-11-20 03:02:12 +0100

    Added toke refresh request.

--- DataModel/DTOs/AuthResponse.cs
+++ DataModel/DTOs/AuthResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OceanBattle.DataModel.DTOs
+﻿namespace OceanBattle.DataModel.DTOs
 {
     /// <summary>
     /// Pair of tokens issued after succesfull auth.
