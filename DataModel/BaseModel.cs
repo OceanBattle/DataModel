8b41b8a21a23d586aa4c34095bc7f168b15f0fd2
kurowskitomek <tomekkurowski2003@gmail.com>
2022-11-27 00:01:36 +0100

    Readded accidentally removed files.

---
+++ DataModel/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OceanBattle.DataModel
+{
+    /// <summary>
+    /// Base class for all databse models.
+    /// </summary>
+    public class BaseModel
+    {
+        /// <summary>
+        /// Model key.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
---
+++ DataModel/DTOs/TokenRefreshRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OceanBattle.DataModel.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for refreshing JWT.
+    /// </summary>
+    public class TokenRefreshRequest
+    {
+        /// <summary>
+        /// Expired (or simply requested to be refreshed) JSON Web Token.
+        /// </summary>
+        [Required(ErrorMessage = "Bearer token is required.")]
+        public string? BearerToken { get; set; }
+
+        /// <summary>
+        /// Refresh token.
+        /// </summary>
+        [Required(ErrorMessage = "Refresh token is required.")]
+        public string? RefreshToken { get; set; }
+    }
+}
