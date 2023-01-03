c806e9040cbc63c58317b3311566aca207b32a97
kurowskitomek <tomekkurowski2003@gmail.com>
2023-01-03 21:55:30 +0100

    Fixed problems with getting levels.

---
+++ DataModel/DTOs/LevelDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OceanBattle.DataModel.DTOs
+{
+    public class LevelDto
+    {
+        public int BattlefieldSize { get; set; }
+        public Dictionary<string, int>? AvailableTypes { get; set; }
+    }
+}
